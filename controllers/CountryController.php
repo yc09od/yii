4055b8145b5c1b2b5e5850eccdf6f00b408f9d5e
horst <yanglongcui@gmail.com>
2016-09-02 01:37:39 -0400

    correct hand write

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -2,7 +2,6 @@
 /**
  * Created by PhpStorm.
  * User: horst
-<<<<<<< HEAD
  * Date: 9/2/16
  * Time: 12:48 AM
  */
@@ -11,25 +10,12 @@ namespace app\controllers;
 
 use yii;
 use yii\base\Controller;
-=======
- * Date: 9/1/16
- * Time: 11:47 PM
- */
-namespace app\controllers;
-
-use Yii;
-use yii\web\Controller;
->>>>>>> 5c2cd6df72fca0bcc78a9902047e490d28aeaca2
 use yii\data\Pagination;
 use app\models\Country;
 
 class CountryController extends Controller{
 
     public function actionIndex(){
-<<<<<<< HEAD
-
-=======
->>>>>>> 5c2cd6df72fca0bcc78a9902047e490d28aeaca2
         $query = Country::find();
 
         $pagination = new Pagination(
@@ -39,7 +25,6 @@ class CountryController extends Controller{
             ]
         );
 
-<<<<<<< HEAD
         $countries = Country::find()->orderBy('name')
             ->offset($pagination->getOffset())
             ->limit($pagination->getLimit())
@@ -54,26 +39,10 @@ class CountryController extends Controller{
         );
 
     }
-=======
-        $countries = $query->orderBy('name')
-            ->offset($pagination->offset)
-            ->limit($pagination->limit)
-            ->all();
-
-        return $this->render('index',
-        [
-            'countries' => $countries,
-            'pagination' => $pagination
-        ]);
-
-    }//end actionIndex
->>>>>>> 5c2cd6df72fca0bcc78a9902047e490d28aeaca2
 
 }
 
 
-<<<<<<< HEAD
+
 ?>
-=======
-?>
->>>>>>> 5c2cd6df72fca0bcc78a9902047e490d28aeaca2
+
