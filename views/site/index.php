5e692ea30c4fa7c1fb97a2d63947c123f5ea15e8
horst <yanglongcui@gmail.com>
2016-09-11 21:19:30 -0400

    add img slider to home page

--- views/site/index.php
+++ views/site/index.php
@@ -4,6 +4,53 @@
 
 $this->title = 'My Yii Application';
 ?>
+
+
+
+<div class="row">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item active"> <img src="http://lorempixel.com/1200/400/sports" style="width:100%" alt="First slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Slide 1</h1>
+                        <p>Aenean a rutrum nulla. Vestibulum a arcu at nisi tristique pretium.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item"> <img src="http://lorempixel.com/1200/400/people" style="width:100%" data-src="" alt="Second    slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Slide 2</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae egestas purus. </p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item"> <img src="http://lorempixel.com/1200/400/abstract" style="width:100%" data-src="" alt="Third slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Slide 3</h1>
+                        <p>Donec sit amet mi imperdiet mauris viverra accumsan ut at libero.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a> </div>
+
+</div>
+
+
+
 <div class="site-index">
 
     <div class="jumbotron">
