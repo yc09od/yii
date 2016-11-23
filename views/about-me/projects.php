edf200168ad8ce818ec300457e75456b63b78138
horst <yanglongcui@gmail.com>
2016-11-23 00:22:32 -0500

    (#13) Create Projects page,Make left nav bar and some resume changes, add text color to site.css

--- views/about-me/projects.php
+++ views/about-me/projects.php
@@ -11,5 +11,32 @@ AppAsset::addCss($this,"@web/css/page/about-me/project.css");
 ?>
 
 <div id = "project">
+    <div class="row top-background">
+    </div>
+    <div class="container">
+        <div class="row" style="padding-top: 20px">
+            <div class="col-lg-10 col-lg-offset-1 board">
+                <div class="row">
 
+                    <div class="col-lg-3" style="background-color: #181818;min-height: 500px">
+                        <div class="row">
+                            <h3 class="text-center text-dark-grey" style="background-color: #111111;padding: 20px;margin: 0">Project List</h3>
+                            <div class="btn-group-vertical" style="width: 100%">
+                                <button class="btn-side-nav" type="button" style="width: 100%">1</button>
+                                <button class="btn-side-nav" type="button" style="width: 100%">1</button>
+                                <button class="btn-side-nav" type="button" style="width: 100%">1</button>
+                                <button class="btn-side-nav" type="button" style="width: 100%">1</button>
+                                <button class="btn-side-nav" type="button" style="width: 100%">1</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-lg-7"></div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 </div>
